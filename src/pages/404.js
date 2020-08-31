0d3fd2c08016833c53a937a1187c2717e597d65e
Ana Claudia Vignola <ana.vignola@b4b.ventures>
2020-08-31 03:21:43 -0300

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
+import media from 'styled-media-query';
 
-import Layout from '../components/Layout';
 import SEO from '../components/seo';
+import GlobalStyles from '../styles/global';
+
+const Container = styled.section`
+  align-items: center;
+  background-image: url('https://anacvignola.dev/assets/images/404.gif');
+  background-position: bottom left;
+  background-repeat: no-repeat;
+  background-size: 800px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  padding: 0 20px;
+  width: 100vw;
+
+  ${media.lessThan("large")`
+    background-size: 600px;
+  `}
+
+  ${media.lessThan("medium")`
+    background-size: 280px;
+  `}
+`
+
+const Title = styled.h1`
+  color: #ff5555;
+  font-size: 120px;
+  font-weight: bold;
+  letter-spacing: 0.1em;
+`
+
+const Text = styled.p`
+  color: #f8f8f2;
+  font-family: Courier, monospace;
+`
 
 const NotFoundPage = () => (
-  <Layout>
+  <Container>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+    <GlobalStyles />
+    <Title>404</Title>
+    <Text>Ué? Cadê? Parece que não tem o que você procura.</Text>
+  </Container>
+)
 
 export default NotFoundPage;
